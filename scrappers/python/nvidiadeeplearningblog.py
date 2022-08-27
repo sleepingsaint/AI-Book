963d16d3e3e2e7b9908a96c14b106dba76904fc3
Santosh Kumar <35530053+sleepingsaint@users.noreply.github.com>
2022-08-27 23:07:56 +0530

    Update nvidiadeeplearningblog.py

--- scrappers/python/nvidiadeeplearningblog.py
+++ scrappers/python/nvidiadeeplearningblog.py
@@ -94,7 +94,8 @@ class NvidiaDeepLearningBlogClient(ResourceClient):
         if load_more_btn is not None:
             self.actions.move_to_element(load_more_btn).perform()
             load_more_btn.click()
-#             self.getResources(num_posts) 
+            if num_posts < 500:
+                self.getResources(num_posts) 
 
 if __name__ == "__main__":
     title = "Nvidia Deep Learning Blog"
