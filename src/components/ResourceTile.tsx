3190886fa7bb3df950ba2c8e6ce0123348a3d35c
sleepingsaint <suryasantosh14523@gmail.com>
2022-09-12 00:38:15 +0530

    added link according to the device form factor

--- src/components/ResourceTile.tsx
+++ src/components/ResourceTile.tsx
@@ -9,32 +9,36 @@ const ResourceTile: React.FC<{ resource: ResourceType }> = ({ resource }) => {
   const { resource: res, setResource } = useResource();
   const selectedStyle = res && res.resource_db_id === resource.resource_db_id ? " bg-blue-100" : "";
   return (
-    <div
-      className={"group p-2 shadow-md my-2 cursor-pointer" + selectedStyle}
-      onClick={() => setResource({ ...resource })}
-    >
-      <p className="mb-2 font-bold">{resource.title}</p>
-      {resource.authors && (
-        <p className="truncate" title={resource.authors}>
-          <FiUsers className="inline-block mr-2" /> {resource.authors}
-        </p>
-      )}
-      {resource.tags && (
-        <p className="truncate" title={resource.tags}>
-          <AiOutlineTags className="inline-block mr-2" /> {resource.tags}
-        </p>
-      )}
-      {resource.publishedOn && (
-        <p className="flex justify-between mt-1">
-          <div>
+    <div className="group relative">
+      <div
+        className={"p-2 shadow-md my-2 cursor-pointer" + selectedStyle}
+        onClick={() => setResource({ ...resource })}
+      >
+        <p className="mb-2 font-bold">{resource.title}</p>
+        {resource.authors && (
+          <p className="truncate" title={resource.authors}>
+            <FiUsers className="inline-block mr-2" /> {resource.authors}
+          </p>
+        )}
+        {resource.tags && (
+          <p className="truncate" title={resource.tags}>
+            <AiOutlineTags className="inline-block mr-2" /> {resource.tags}
+          </p>
+        )}
+        {resource.publishedOn && (
+          <p>
             <MdDateRange className="inline-block mr-2" /> {new Date(resource.publishedOn).toDateString()}
-          </div>
-          <a className="hidden group-hover:inline mr-2 bg-blue-200 px-2" target="_blank" href={resource.url}>
-            <FiExternalLink className="inline mr-1" />
-            <span>Open</span>
-          </a>
-        </p>
-      )}
+          </p>
+        )}
+      </div>
+      <a
+        className="md:hidden absolute bottom-2 right-0 group-hover:block mr-2 bg-blue-200 px-2"
+        target="_blank"
+        href={resource.url}
+      >
+        <FiExternalLink className="inline mr-1" />
+        <span>Open</span>
+      </a>
     </div>
   );
 };
