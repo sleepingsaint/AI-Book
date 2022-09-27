5751b2b0544bd5f13f3e78c19aee2de266de3ea0
sleepingsaint <suryasantosh14523@gmail.com>
2022-09-27 18:57:29 +0530

    added social client class

--- utils/python/resource_client.py
+++ utils/python/resource_client.py
@@ -2,9 +2,9 @@ import os
 from datetime import datetime
 from typing import List, Union
 from utils.python.db_client import NotionClient
-from discord_webhook import DiscordWebhook
+from utils.python.social_client import SocialClient
 
-class ResourceClient:
+class ResourceClient(SocialClient):
     def __init__(self, source: str, dateFormat: str) -> None:
         self.db = NotionClient()
         self.source = source 
@@ -16,7 +16,6 @@ class ResourceClient:
         self.new_source = not self.db.sourceExists(source)
         self.refetch = self.refetch or self.new_source
         self.delete = int(os.environ.get("DELETE", 0)) == 1
-        self.discord_webhook_url = os.environ.get("DISCORD_WEBHOOK_URL", None)
     
     def formatTitle(self, title: str) -> str:
         return title.strip()
@@ -53,13 +52,3 @@ class ResourceClient:
         if type(tags) is str:
             tags = tags.strip().split(',')
         return tags
-    
-    def discordSendResourceNotification(self, resource_url):
-        if self.discord_webhook_url is not None:
-            webhook = DiscordWebhook(url=self.discord_webhook_url, content=resource_url, rate_limit_retry=True)
-            webhook.execute()
-    
-    def discordSendSourceNotification(self, source, url):
-        if self.discord_webhook_url is not None and self.new_source:
-            webhook = DiscordWebhook(url=self.discord_webhook_url, rate_limit_retry=True, content=f"[{source} Added]({url})")
-            webhook.execute()
---
+++ utils/python/social_client.py
@@ -0,0 +1,47 @@
+import os
+from discord_webhook import DiscordWebhook
+
+class SocialClient:
+    def __init__(self) -> None:
+        # discord keys
+        self.discord_sources_webhook_url = os.environ.get("DISCORD_SOURCES_WEBHOOK_URL", None)
+        self.discord_resources_webhook_url = os.environ.get("DISCORD_RESOURCES_WEBHOOK_URL", None)
+
+        # twitter keys
+        
+        # reddit keys
+    
+    def sendSourceNotification(self, source, url):
+        self.sendDiscordSourceNotification(source, url)
+        self.sendRedditSourceNotification(source, url)
+        self.sendTwitterSourceNotification(source, url)
+
+    def sendResourceNotification(self, resource_url):
+        self.sendDiscordResourceNotification(resource_url)
+        self.sendRedditResourceNotification(resource_url)
+        self.sendTwitterResourceNotification(resource_url)    
+
+    # discord notification handlers
+    def sendDiscordSourceNotification(self, source, url):
+        if self.discord_sources_webhook_url is not None:
+            webhook = DiscordWebhook(url=self.discord_sources_webhook_url, rate_limit_retry=True, content=f"[{source} Added]({url})")
+            webhook.execute()
+    
+    def sendDiscordResourceNotification(self, resource_url):
+        if self.discord_resources_webhook_url is not None:
+            webhook = DiscordWebhook(url=self.discord_resources_webhook_url, content=resource_url, rate_limit_retry=True)
+            webhook.execute()
+
+    # reddit notification handlers
+    def sendRedditSourceNotification(self, source, url):
+        pass
+    
+    def sendRedditResourceNotification(self, resource_url):
+        pass
+
+    # twitter notification handlers
+    def sendTwitterSourceNotification(self, source, url):
+        pass
+    
+    def sendTwitterResourceNotification(self, resource_url):
+        pass
